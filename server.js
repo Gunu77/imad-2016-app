111c852e303cc5fcccee8c27f1ed3afa9827f4f2
Gunu77 <singhgundeep5620@gmail.com>
2016-09-21 20:25:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/supernatural.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'supernatural.png'));
+});
+
 
 
 
