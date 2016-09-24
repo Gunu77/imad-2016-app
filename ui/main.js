a7eddac55617c81921f011c7a03a9fcf12b162b3
Gunu77 <singhgundeep5620@gmail.com>
2016-09-24 11:54:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,26 @@
-console.log('Loaded!');
+var button=document.getElementById("Counter");
+var counter=0;
+
+button.onclick=function() {
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        
+        if(request.readystate===XMLHttpRequest.DONE){
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById("Count");
+              span.innerHTML=counter.toString();
+          }  
+        }
+        
+    request.open('GET','http://gunu77.imad.hasura-app.io/counter',true);
+    request.send(null);
+    };
+   
+   
+   
+   
+   
+};
+
