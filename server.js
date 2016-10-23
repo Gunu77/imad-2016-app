73193a1ef564d9bca492512bacde111ae0f83612
Gunu77 <singhgundeep5620@gmail.com>
2016-10-23 15:51:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.get('/', function (req, res) {
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result {
+    pool.query('SELECT * FROM test',function(err,result) {
         if(err){
             res.status(500).send(err.toString());
         }else{
-            res.
+            res.send(JSON.stringify(result));
         }
-    }))
+    });
 });
 
 app.get('/about', function (req, res) {
