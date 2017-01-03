0e817ecc1c20594f0aa20e8dc6751616c7ffbb6f
Gunu77 <singhgundeep5620@gmail.com>
2017-01-03 22:30:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/gs.html', function (req, res) {
-  res.sendFile(path.join(__dirname, '', 'gs.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'gs.html'));
 });
 
 app.get('/about.html', function (req, res) {
-  res.sendFile(path.join(__dirname, '', 'about.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
 
 var pool=new Pool(config);
