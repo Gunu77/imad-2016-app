f603d5ab902e6d2b911d6a8f57226e2867ceb27c
Gunu77 <singhgundeep5620@gmail.com>
2017-01-04 17:41:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/', 'gs.html'));
 });
 
-app.get('/login', function (req, res) {
+app.get('/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, '/', 'main.js'));
 });
 
