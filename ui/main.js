9a15b50b4ddc509c3b7606e4cd479eeb3b8a88e4
Gunu77 <singhgundeep5620@gmail.com>
2017-01-04 15:32:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-console.log('Loaded!');
+
+var submit=document.getElementById('sybmit_btn');
+submit.onclick=function() {
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function() {
+        
+        
+        
+    }
+    
+    
+}
