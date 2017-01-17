c51f8b469093476fbb906f90e03045a17c7d7af5
Gunu77 <singhgundeep5620@gmail.com>
2017-01-17 12:08:20 +0530

    [imad-console] Updates main.js

--- main.js
+++ main.js
@@ -1,33 +1,124 @@
 
-var submit=document.getElementById('submit_btn');
- submit.onclick=function() {
-     console.log("i am in");
-    
-    var request=new XMLHttpRequest();
-    
-    request.onreadystatechange=function() {
-        
-        if(request.readyState===XMLHttpRequest.DONE) {
-            console.log("Gunu is great");
-            if(request.status===200) {
-                console.log("I am a good boy");
+//submit username and password to login
+
+var submit = document.getElementById('login_btn');
+submit.onclick = function () {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                //capture a list of names and render it as a list
                console.log('user logged in');
-               alert('Logged in successfully');
-            } else if(request.status === 403) {
-                alert('Username/password is incorrect');
-            } else if(request.status === 500) {
-                alert('Something went wrong on the server');
+               alert('Congrats!!The magic has been UNLOCKED!');
+               } else if (request.status === 403) {
+                   alert('uh-oh! wrong username or password :(');
+               } else if (request.status === 500) {
+                   alert('something went wrong with the magic wand');
+               }
+               loadLogin();
             }
-            
-        }
-        
+        //not done yet
     };
-    var username=document.getElementById('username').value;
-    var password=document.getElementById('password').value;
+     
+     //make the request
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+     request.open('POST','http://gunu77.imad.hasura-app.io/login', true);
+     request.setRequestHeader('Content-Type', 'application/json');
+     request.send(JSON.stringify({username: username, password: password}));
+   
+};
+
+var register = document.getElementById('register_btn');
+register.onclick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                //capture a list of names and render it as a list
+                alert('User created successfully');
+                register.value = 'Registered!';
+               } else {
+                   alert('Could not register the user');
+                   register.value = 'Register';
+               }
+               
+            }
+        //not done yet
+    };
+    //make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST',' http://gunu77.imad.hasura-app.io/login',true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username: username, password: password }));
+    request.open('POST', 'http://e-crisis.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));  
+    register.value = 'Registering...';
 
+};
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+}
+
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+//submit comment
+
+var submitComment = document.getElementById('submit_comment');
+submitComment.onclick = function () {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                //capture a list of comments and render it as a list
+               var comments = request.responseText;
+               comments = JSON.parse(comments);
+               var list = '';
+               for (var i=0; i<comments.length; i++) {
+                   list += '<li>' + comments[i] + '</li>';
+                   
+               }
+               var uli = document.getElementById('commentList');
+               uli.innerHTML = list;
+            }
+        }
+    };
+    
+     //make the request
+     var commentInput = document.getElementById('comment');
+     var comment = commentInput.value;
+     request.open('GET','http://e-crisis.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+     request.send(null);
+    
 };
