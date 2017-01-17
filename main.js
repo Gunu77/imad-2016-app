c1232c749f177fd5609df1c36549f922b306527e
Gunu77 <singhgundeep5620@gmail.com>
2017-01-17 14:44:50 +0530

    [imad-console] Updates main.js

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ function loadLoginForm () {
         <input type="submit" id="register_btn" value="Register" />
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
-
-// Submit username/password to login
-var submit = document.getElementById('login_btn');
-submit.onclick = function () {
-     // Create a request object
-    var request = new XMLHttpRequest();
     
-    // Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            // Take some action
-            if (request.status === 200) {
-                submit.value = 'Sucess!';
-            } else if (request.status === 403) {
+    // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
@@ -33,21 +33,22 @@ submit.onclick = function () {
               }
               loadLogin();
           }  
-        // Not done yet
+          // Not done yet
         };
-    
-    // Make the request
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-   request.open('POST', '/login', true);
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
-
-};
- var register = document.getElementById('register_btn');
+        
+    };
+    
+    var register = document.getElementById('register_btn');
     register.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
@@ -65,7 +66,8 @@ submit.onclick = function () {
               }
           }
         };
-     // Make the request
+        
+        // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
@@ -77,6 +79,7 @@ submit.onclick = function () {
     
     };
 }
+
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
@@ -101,6 +104,7 @@ function loadLogin () {
     request.open('GET', '/check-login', true);
     request.send(null);
 }
+
 function loadArticles () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
@@ -115,10 +119,10 @@ function loadArticles () {
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
                     (${articleData[i].date.split('T')[0]})</li>`;
                 }
-                content += "</ul>";
+                content += "</ul>"
                 articles.innerHTML = content;
             } else {
-                articles.innerHTML=('Oops! Could not load all articles!');
+                articles.innerHTML('Oops! Could not load all articles!')
             }
         }
     };
