7e9f789c67f2a9c6ad4c8879dfc415c4c16f7d32
Gunu77 <singhgundeep5620@gmail.com>
2017-01-18 18:31:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post('/login', function (req, res) {
             res.status(500).send(err.toString());
         }else {
             if (result.rows.length===0){
-                res.send(403).send('usename/password is invalid');
+                res.status(403).send('usename/password is invalid');
             }else {
                 var dbString=result.rows[0].password;
                var salt=dbString.split('$')[2];
@@ -94,7 +94,7 @@ app.post('/login', function (req, res) {
                     req.session.auth = {userId: result.rows[0].id};
                res.send('credentials correct!'); 
                }else {
-                    res.send(403).send('usename/password is invalid');
+                    res.status(403).send('usename/password is invalid');
                
                }
             }
