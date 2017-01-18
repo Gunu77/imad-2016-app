7dffe781cacf7a55b9732d951bc12bed87be1d94
Gunu77 <singhgundeep5620@gmail.com>
2017-01-18 11:13:53 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,76 @@
 
-var submit=document.getElementById('submit_btn');
- submit.onclick=function() {
-    
-    var request=new XMLHttpRequest();
-    
-    request.onreadystatechange=function() {
-        
-        if(request.readyState===XMLHttpRequest.DONE) {
-            
-            if(request.status===200) {
-                
-               console.log('user logged in');
-               alert('Logged in successfully');
-            } else if(request.status === 403) {
-                alert('Username/password is incorrect');
-            } else if(request.status === 500) {
-                alert('Something went wrong on the server');
-            }
-            
-        }
-        
-    };
-    var username=document.getElementById('username').value;
-    var password=document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    request.open('POST',' http://gunu77.imad.hasura-app.io/login',true);
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username: username, password: password }));
+console.log('Loaded!');
 
+
+
+//submit name
+window.onload = function () {
+
+var submit=document.getElementById('login_btn');
+submit.onclick=function() {
+     //create a response
+   var request=new XMLHttpRequest();
+   //capture the response
+   request.onreadystatechange=function(){
+       if(request.readyState===XMLHttpRequest.DONE){
+           if(request.status===200){
+                //capture response
+   console.log('user logged in');
+   alert('logged in successfully');
+               
+           }else if (request.status===403){
+               alert('username/password is correct');
+           }else if (request.status===500){
+               alert('something went wrong');
+           
+           }
+           
+       }
+   };
+
+   //Make a request
+   var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+   request.open('POST','http://gunu77.imad.hasura-app.io/login',true);
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({username: username,password: password}));
+};
+};
+
+   //signup
+   
+ window.onload = function () {
+
+   var register=document.getElementById('register_btn');
+   
+ register.onclick =function() {
+    
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          
+        }
+        };
+        // Make the request
+        var username = document.getElementById('user').value;
+        var password = document.getElementById('pass').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', 'http://gunu77.imad.hasura-app.io/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username:username, password: password}));  
+        }; 
 };
