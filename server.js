a6575599f817d1bb8a1bef56c977efeaa0fd00c5
Gunu77 <singhgundeep5620@gmail.com>
2017-01-19 11:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -174,6 +174,10 @@ app.get('/register.html', function (req, res) {
   res.sendFile(path.join(__dirname, '/', 'register.html'));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
