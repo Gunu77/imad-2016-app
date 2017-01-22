55f81671281338470ab9c868d194033dc3334358
Gunu77 <singhgundeep5620@gmail.com>
2017-01-22 11:27:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, '/', 'main.js'));
+});
+
 function hash(input, salt) {
     var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return['pbkdf2','10000',salt, hashed.toString('hex')].join('$');
